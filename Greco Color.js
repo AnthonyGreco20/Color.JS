7cfa92493a8d9cffdce1591ec9e9889a64ffa50e
AnthonyGreco20 <9agreco@stpatrick.org>
2016-11-09 13:57:48 -0600

    Color Project
    Worked on compliment

---
+++ Greco Color.js
@@ -0,0 +1,48 @@
+//Declare the Color Object with our new keyword below here.
+const Color = newObject (r,g,b);
+/*
+** rgb is an array that hold the color values.
+** first is red -> R, then green -> G, and blue -> B
+** Example
+*/
+Color.isColor = function(rgb){
+  //each color must be between 0 and 255. Also can be equal.
+  let redworks = (rgb[0] >= 0) && (rgb[0] <= 255);
+  let greenworks = (rgb[1]) >=0) && (rgb[1]) <= 255);
+  let blueworks = (rgb[2]) >=0) && (rgb[2])
+
+  if(redworks && greenworks && blueworks){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
+Color.redIntensity = function(rgb){
+  let redIntensity = (rgb[0]/255)*100
+  redIntensity = Math.Round(redIntensity)
+};
+//greenIntensity
+Color.greenIntensity = function(rgb){
+  let greenIntensity = (rgb[1]/255)*100
+  greenIntensity = Math.Round(greenIntensity)
+}
+//blueIntensity
+Color.blueIntensity = function(rgb){
+  let blueIntensity = (rgb[1]/255)*100
+  blueIntensity = Math.Round(blueIntensity)
+}
+//brightness
+Color.brightness = function(){
+  var bright;
+  bright = (Color.blueIntensity(rgb)+Color.redIntensity(rgb)+Color.greenIntensity(rgb))/3;
+  return bright;
+}
+//complement
+Color.compliment=function(){
+  var compliment=rgb;
+  rgb[0]=255-rgb[0];
+  rgb[1]=255-rgb[1];
+  rgb[2]=255-rgb[2];
+  return compliment;
+};
